567cc15e1239b9dbe3395d9b1d555e003cfd7535
Muhammad Salif <muhammad.salif@yahoo.com>
2021-01-10 19:57:45 +0500

    Login Signup API's done.

--- app.js
+++ app.js
@@ -4,20 +4,56 @@ const http = require("http");
 const app = express()
 const socketIO = require('socket.io');
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt-inzi")
+
 
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 /////////////////////////////////////////////////////////////////////////
 // Mongoose connections
-const dbURI= "mongodb://tweetsdbuser:tweetsdbpassword@cluster0-shard-00-00.em7xy.mongodb.net:27017,cluster0-shard-00-01.em7xy.mongodb.net:27017,cluster0-shard-00-02.em7xy.mongodb.net:27017/admin?ssl=true&replicaSet=atlas-o043up-shard-0&readPreference=primary&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1&3t.uriVersion=3&3t.connection.name=Tweets+Database&3t.databases=admin,tweetsdbuser&3t.alwaysShowAuthDB=true&3t.alwaysShowDBFromUserRole=true&3t.sslTlsVersion=TLS"
+let dbURI = "mongodb+srv://dbuser:dbpassword@cluster0.lxign.mongodb.net/tweetsdatabase?retryWrites=true&w=majority"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(dbURI, {useNewUrlParser: true});
+mongoose.connection.on("connected", () => {
+    console.log("Mongoose is connected")
+})
 
+mongoose.connection.on("disconnected", () => {
+    console.log("Mongoose disconnected")
+    process.exit(1);
+})
 
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
 
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+/////////////////////////////////////////////////////////////////////////
+// Db Schemas
 
+var userSchema = new mongoose.Schema({
+    userName: { type: String },
+    password: { type: String },
+    email: { type: String }
+})
+var users = mongoose.model("users", userSchema);
 
+var tweetsSchema = mongoose.Schema({
+    tweets: {
+        type: String,
+        // required: true,
+        // unique: true,
+    },
+})
+var tweets = mongoose.model("tweets", tweetsSchema);
 
 /////////////////////////////////////////////////////////////////////////
 const server = http.createServer(app)
@@ -27,11 +63,6 @@ const io = socketIO(server, {
         origin: "*"
     },
 })
-
-app.post("/signup", (req, res) => {
-    
-})
-
 
 app.get("/", (req, res) => {
     res.status(200).send({
@@ -46,11 +77,104 @@ app.get("/", (req, res) => {
     })
 })
 
-app.post("/tweet", (req, res) => {
-    req
-
+app.post("/signup", (req, res) => {
+    if (!req.body || !req.body.email || !req.body.userName || !req.body.password) {
+        res.status(403).send(`
+        please provide userName, password in json body.
+        e.g:
+        {
+            "name": "Mark",
+            "email": "abc@abc.com",
+            "password": "abc",
+        }`)
+    } else {
+        users.findOne({ email: req.body.email }, (err, doc) => {
+            if (!doc) {
+                bcrypt.stringToHash(req.body.password)
+                    .then(passwordHash => {
+                        console.log("hash: ", passwordHash);
+                        users.create({
+                            userName: req.body.userName,
+                            password: passwordHash,
+                            email: req.body.email
+                        }, function (err, success) {
+                            if (err) {
+                                console.log("Internal error", err)
+                                res.status(500).send({
+                                    message: "Internal Error"
+                                })
+                            }
+                            if (success) {
+                                console.log("User created successfully ", success)
+                                res.status(200).send({
+                                    message: "User created successfully"
+                                })
+                            }
+                        })
+                    })
+            }
+            if (doc) {
+                console.log("Email already exists please try with another email")
+                res.status(409).send({
+                    message: "Email already exists please try with another email"
+                })
+            }
+            if (err) {
+                console.log("Internal Error")
+                res.status(500).send({
+                    message: "Internal Error"
+                })
+            }
+        })
+    }
 })
 
+app.post("/login", (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        res.status(403).send(`
+        please provide userName, password in json body.
+        e.g:
+        {
+            "email": "abc@abc.com",
+            "password": "1234",
+        }`)
+    }
+
+    users.findOne({ email: req.body.email }, (err, doc) => {
+        if (doc) {
+            bcrypt.varifyHash(req.body.password, doc.password)
+                .then(isMatched => {
+                    if (isMatched) {
+                        res.status(200).send({
+                            message: "Login  successfully"
+                        })
+                        console.log(req.body.email, "Login successfully");
+                    } else {
+                        res.status(403).send({
+                            message: "Password is incorrect"
+                        })
+                        console.log("Password Hash is not matched");
+                    }
+                })
+                .catch(e => {
+                    res.status(500).send({
+                        message: "Internal Error"
+                    })
+                    console.log("Internal error ", e)
+                })
+        }
+        if (!doc) {
+            res.status(404).send({
+                message: "Email does not exists kindly signup first"
+            })
+        }
+        if (err) {
+            res.status(500).send({
+                message: "Internal Error"
+            })
+        }
+    })
+})
 
 
 
